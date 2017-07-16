1bb8488191cbc0fbe7c38a4a481cc4daf308264a
efrenmanuel <efrenmanuelgonzalez@gmail.com>
2017-07-16 16:07:09 +0200

    Add files via upload

---
+++ dic creator 1.py
@@ -0,0 +1,8 @@
+import json, os
+dic={}
+with open("answ.json","w") as f:
+  json.dump(dic, f, indent=4)
+with open("p1.json","w") as f:
+  json.dump(dic, f, indent=4)
+with open("lastmsg.json","w") as f:
+  json.dump(dic, f, indent=4)
---
+++ learntest.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+import json, numpy, decimal
+est=1
+while 1:
+ 
+    import AI
+    AI.learn((input("You: ")).replace("{","$$*$").replace("}","$$*¿").replace("\"","$$&$").replace("\'","$$¿$"),"000",est)
+   
+    print("learned")
